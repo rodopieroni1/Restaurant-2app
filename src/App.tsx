2131dadf5268ac0966aac8ac392e5e7dd99ee6e2
Rodrigo Pieroni <rodrigo.pieroni@possumus.tech>
2022-06-03 12:53:25 -0300

    comit de todo

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Details } from "./pages/details";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+
+function App() {
+  return <div>ver error</div>;
+}
+
+export default App;
